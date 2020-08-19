51dca67703310db2024a1ad31ab05d221e1d7d02
Tom Raistrick <56520365+tom-raistrick@users.noreply.github.com>
2020-08-19 11:18:24 +0100

    Update 'back_end.py' and 'romanised'

--- back_end.py
+++ back_end.py
@@ -1,8 +1,10 @@
 from random import randint
 
 syllabary_valid = False
+
 while not syllabary_valid:
     syllabary = input("Choose characters to display by typing 'hiragana', 'katakana', or 'both': ")
+    print('\n')
     if syllabary == 'exit':
         exit()
     elif syllabary == 'both':
@@ -13,24 +15,25 @@ while not syllabary_valid:
         kana_list = [line.rstrip('\n') for line in open('resources/' + syllabary)]
         syllabary_valid = True
     else:
-        print('Selection invalid.')
+        print('Selection invalid.\n')
 
 run, new_prompt = True, True
-romanised_list = [line.rstrip('\n') for line in open('resources/romanised')]
+romanised_list = [line.rstrip('\n') for line in open('resources/romanised')] * 2
 
 while run:
     if new_prompt:
-        num = randint(0, len(kana_list) - 1)
-    current_kana = kana_list[num]
+        kana_num = randint(0, len(kana_list) - 1)
+    current_kana = kana_list[kana_num]
     print(current_kana)
     search_input = input('Rōmaji: ')
     if search_input == 'exit':
         exit()
     elif search_input == 'skip':
+        print("Answer was: '" + romanised_list[kana_num] + "'.\n")
         new_prompt = True
-    elif search_input == romanised_list[num]:
-        print('Correct!')
+    elif search_input in romanised_list[kana_num]:
+        print('Correct!\n')
         new_prompt = True
     else:
-        print('Incorrect.  Please try again.')
+        print('Incorrect.  Please try again.\n')
         new_prompt = False
